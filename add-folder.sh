a730421ea59c807b6271aa8e76a0c722487f58af
wouldgo <wouldgo84@gmail.com>
2015-09-11 00:22:07 +0200

    exec instead of nsenter

--- add-folder.sh
+++ add-folder.sh
@@ -11,30 +11,35 @@ fi
 set -e
 HOSTPATH=$1
 CONTPATH=$2
+TMPDIR='/tmpmnt'
 REALPATH=$(readlink --canonicalize $HOSTPATH)
 FILESYS=$(df -P $REALPATH | tail -n 1 | awk '{print $6}')
 
 while read DEV MOUNT JUNK
 do [ $MOUNT = $FILESYS ] && break
-done </proc/mounts
-echo $DEV
+done < /proc/mounts
 [ $MOUNT = $FILESYS ] # Sanity check!
 
 while read A B C SUBROOT MOUNT JUNK
 do [ $MOUNT = $FILESYS ] && break
 done < /proc/self/mountinfo
-echo $SUBROOT
 [ $MOUNT = $FILESYS ] # Moar sanity check!
 
 SUBPATH=$(echo $REALPATH | sed s,^$FILESYS,,)
 DEVDEC=$(printf "%d %d" $(stat --format "0x%t 0x%T" $DEV))
 
-#echo "$DEV - $DEVDEC - $DEV - /tmpmnt/$SUBROOT/$SUBPATH - $CONTPATH"
-# docker-enter nginx -- sh -c \
-#   "[ -b $DEV ] || mknod --mode 0600 $DEV b $DEVDEC"
-# docker-enter nginx -- mkdir /tmpmnt
-# docker-enter nginx -- mount $DEV /tmpmnt
-# docker-enter nginx -- mkdir -p $CONTPATH
-# docker-enter nginx -- mount -o bind /tmpmnt/$SUBROOT/$SUBPATH $CONTPATH
-# docker-enter nginx -- umount /tmpmnt
-# docker-enter nginx -- rmdir /tmpmnt
+if [[ $SUBROOT = '/' ]]; then
+
+  BINDDIR="$TMPDIR$SUBPATH";
+else
+
+  echo "not supported by now...";
+fi
+
+docker exec -it nginx sh -c "[ -b $DEV ] || mknod --mode 0600 $DEV b $DEVDEC"
+docker exec -it nginx mkdir $TMPDIR
+docker exec -it nginx mount $DEV $TMPDIR
+docker exec -it nginx mkdir -p $CONTPATH
+docker exec -it nginx mount -o bind $TMPDIR/$SUBROOT/$SUBPATH $CONTPATH
+docker exec -it nginx umount $TMPDIR
+docker exec -it nginx rmdir $TMPDIR
--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker run \
-  --rm \
-  -v /usr/local/bin:/target \
-  jpetazzo/nsenter && \
-
 docker build \
   --tag 720kb/nginx \
   --force-rm \
