4d2adfb4742082b1094555e430743655989954f5
wouldgo <wouldgo84@gmail.com>
2015-09-12 22:44:27 +0200

    if hostpath is not empty the container data will be overwritten

--- add-folder.sh
+++ add-folder.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 #Thanks to http://jpetazzo.github.io/2015/01/13/docker-mount-dynamic-volumes/
-
 if [ $# -eq 0 ] || [ $# -eq 1 ]; then
 
   echo "Not correct arguments supplied. Usage: ./add-folder.sh <host-path> <container-path> <stop-nginx>" && \
@@ -30,6 +29,7 @@ SUBPATH=$(echo $REALPATH | sed s,^$FILESYS,,)
 DEVDEC=$(printf "%d %d" $(stat --format "0x%t 0x%T" $DEV))
 
 if [[ $TO_STOP ]]; then
+
   echo "Stopping nginx inside the container" && \
   docker exec -it nginx nginx -s stop
 fi
@@ -38,14 +38,26 @@ docker exec -it nginx bash -c "[ -b $DEV ] || mknod --mode 0600 $DEV b $DEVDEC"
 docker exec -it nginx bash -c "mkdir $TMPDIR"
 docker exec -it nginx bash -c "mount $DEV $TMPDIR"
 docker exec -it nginx bash -c "mkdir -p /tmp/swap_folder"
-docker exec -it nginx bash -c "[ $(find $CONTPATH -maxdepth 0 -type d -empty 2>/dev/null) ] && mkdir -p $CONTPATH || mv $CONTPATH/* /tmp/swap_folder"
+
+if [[ $(find $REALPATH -maxdepth 0 -type d -empty 2>/dev/null) ]]; then
+
+  #empty
+  docker exec -it nginx bash -c "[ $(find $CONTPATH -maxdepth 0 -type d -empty 2>/dev/null) ] && mkdir -p $CONTPATH || mv $CONTPATH/* /tmp/swap_folder"
+else
+
+  #not empty
+  docker exec -it nginx bash -c "[ $(find $CONTPATH -maxdepth 0 -type d -empty 2>/dev/null) ] && mkdir -p $CONTPATH || rm $CONTPATH/*"
+fi
+
 docker exec -it nginx bash -c "mount -o bind $TMPDIR/$SUBROOT/$SUBPATH $CONTPATH"
-docker exec -it nginx bash -c "[ $(find /tmp/swap_folder -maxdepth 0 -type d -empty 2>/dev/null) ] || mv /tmp/swap_folder/* $CONTPATH"
+docker exec -it nginx bash -c "[ $(find /tmp/swap_folder -maxdepth 0 -type d -empty 2>/dev/null) ] || mv /tmp/swap_folder/* $CONTPATH 2>/dev/null"
+
 docker exec -it nginx bash -c "rm -Rf /tmp/swap_folder"
 docker exec -it nginx bash -c "umount $TMPDIR"
 docker exec -it nginx bash -c "rmdir $TMPDIR"
 
 if [[ $TO_STOP ]]; then
+
   echo "Restarting nginx inside the container" && \
   docker exec -it nginx nginx
 fi
